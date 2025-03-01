850e9556b50abf080f2eb8ad1b36c7ad1680430e
MillionMedia2 <neil@millionmedia.com>
2025-03-01 16:47:00 +0000

    logo update

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -354,7 +354,7 @@ const Chat = ({
           className={styles.input}
           value={userInput}
           onChange={(e) => setUserInput(e.target.value)}
-          placeholder="Enter your question"
+          placeholder="Ask Plantz a question..."
         />
         <button
           type="submit"
