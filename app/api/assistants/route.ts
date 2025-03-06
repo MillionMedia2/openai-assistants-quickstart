db8a19b40f079b591405b6a9296febc0bf50925c
MillionMedia2 <neil@millionmedia.com>
2025-03-06 19:39:28 +0000

    Adding tool instructions to route.ts,

--- app/api/assistants/route.ts
+++ app/api/assistants/route.ts
@@ -5,13 +5,18 @@ export const runtime = "nodejs";
 // Create a new assistant
 export async function POST() {
   const assistant = await openai.beta.assistants.create({
-    instructions: "You are a helpful assistant.",
-    name: "Quickstart Assistant",
+    instructions: "You are a helpful assistant. When answering questions, please use the File Search tool to find relevant information in the documents provided.",
+    name: "Plantz Agent",
     model: "gpt-4o-mini",
     tools: [
       { type: "code_interpreter" },
       { type: "file_search" },
     ],
+    tool_resources: {
+      file_search: {
+        vector_store_ids: ["vs_67a669ee3c408191b5588e966f605592"]
+      }
+    },
   });
   return Response.json({ assistantId: assistant.id });
 }
